fa4afb1ebd855cbe2f292ebb9792ec2376ba7cee
Jerome Noveda <noveda.jerome@gmail.com>
2015-03-09 18:28:20 +0800

    new file PDOconnector

---
+++ include/PDOConnector.php
@@ -0,0 +1,32 @@
+<?php
+
+class PDOConnector{
+	private $host = "localhost";
+	private $dbname = "db_tPoll";
+	private $username = "root";
+	private $password = "";
+	protected $dbh = null;
+
+	protected function connect(){
+		$host = $this->host;
+		$dbname = $this->dbname;
+		$username = $this->username;
+		$password = $this->password;
+
+		try{
+			$this->dbh = new PDO("mysql:dbname=".$dbname.";host=".$host, $username, $password);
+		}catch(PDOException $e){
+			print_r($e);
+		}
+		
+
+	}
+
+	protected function close(){
+		try{
+			$this->dbh = null;
+		}catch(PDOException $e){
+			print_r($e);
+		}
+	}
+}
