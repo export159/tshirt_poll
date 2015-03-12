44074da3375d2a17477a51527404d5b5cad1db98
Jerome Noveda <noveda.jerome@gmail.com>
2015-03-12 16:40:09 +0800

    counting the votes functionality in model polls

--- model/model_polls.php
+++ model/model_polls.php
@@ -1,7 +1,12 @@
 <?php
 require_once "../include/PDOConnector.php";
+require_once "../model/model_brands.php";
 
 class Model_polls extends PDOConnector{
+	private $model_brands;
+	function __construct(){
+		$this->model_brands = new Model_brands();
+	}
 	/*
 		function addPoll
 		for:      adding polls
@@ -24,5 +29,53 @@ class Model_polls extends PDOConnector{
 		}
 		$this->close();
 		return $id;
+	}
+
+	/*
+		function polls
+		params:    none
+		for:       to count the votes
+		return:    array() <-- multidimension
+	*/
+	function polls(){
+		$result = null;
+		$counter = 0;
+		$brands = $this->model_brands->getAllBrands();
+		try{
+			foreach($brands as $brand){
+				$result[$counter]['id'] = $brand['id'];
+				$result[$counter]['name'] = $brand['name'];
+				$result[$counter]['votes'] = $this->countVotes($brand['id']);
+			}
+		}catch(PDOException $e){
+			print_r($e)
+		}
+		return $result;
+	}
+	/*
+		function countVotes
+		params:    $brand_id
+		for   :    called by function polls()
+		return:    integer
+	*/
+	function countVotes($brand_id){
+		$result = 0;
+		$this->connect();
+		try{
+			$sql = "Select count(user_id) FROM tbl_polls WHERE brand_id = ?";
+			$stmt = $this->dbh->prepare($sql);
+			$stmt->bindParam(1, $brand_id);
+			$stmt->execute();
+
+			if($rs = $stmt->fetch()){
+				$result = $rs[1];
+			}else{
+				$result = 0;
+			}
+		}catch(PDOException $e){
+			print_r($e)
+		}
+		$this->close();
+		return $result;
 	}
 }
