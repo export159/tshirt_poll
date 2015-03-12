6b196b77e9b8e83579c7809159f4f8434ef8ccf0
Jerome Noveda <noveda.jerome@gmail.com>
2015-03-12 15:33:05 +0800

    exported the database

---
+++ database/127_0_0_1 (1).sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.9
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 12, 2015 at 08:32 AM
+-- Server version: 5.5.34
+-- PHP Version: 5.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `db_tpoll`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_brands`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_brands` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+
+--
+-- Dumping data for table `tbl_brands`
+--
+
+INSERT INTO `tbl_brands` (`id`, `name`) VALUES
+(1, 'Levis'),
+(2, 'Tshirt'),
+(3, 'ukay ukay'),
+(4, 'mercado'),
+(5, 'laray'),
+(6, 'sudoy'),
+(7, 'delivery'),
+(8, 'ambot'),
+(9, 'tries'),
+(10, 'test');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_polls`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_polls` (
+  `int` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `brand_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`int`),
+  UNIQUE KEY `user_id` (`user_id`),
+  KEY `brand_id` (`brand_id`,`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_users`
+--
+
+CREATE TABLE IF NOT EXISTS `tbl_users` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`id`, `name`, `username`, `password`) VALUES
+(1, 'Administrator', 'admin', 'admin');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
